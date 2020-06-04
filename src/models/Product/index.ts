bbdbe57964f59a1dcd1f946cd140d94d99c36747
Alfredo V <alfredo.villagomez.m@gmail.com>
2020-06-04 16:43:25 -0400

    models index configured

--- src/models/Product/index.ts
+++ src/models/Product/index.ts
@@ -1,1 +1,2 @@
 export { default } from './Product';
+export { PriceRule } from './Product';
---
+++ src/models/index.ts
@@ -0,0 +1,3 @@
+export { default as Product } from './Product';
+export { default as CarInsurance } from './CarInsurance';
+export { PriceRule } from './Product';
